392ce3e5c11abd937cf4d98d55a8ddd33590eb7d
PyryL <pyry.laaksonen@tuni.fi>
2023-02-06 15:05:58 +0200

    Ex 03 comments

--- Cpp_advanced_round4/Cpp_advanced_round4/ratkaisu_kierros4_teht3.cpp
+++ Cpp_advanced_round4/Cpp_advanced_round4/ratkaisu_kierros4_teht3.cpp
@@ -1,16 +1,18 @@
+//Program asks the user for name and a short description, then creates a html file with the users name as header and description as a paragraph
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 int main() {
-	std::ofstream outFile;
-	outFile.open ("C_html.html");
+	std::ofstream outFile; //Define the ofstream as outFile named variable
+	outFile.open ("C_html.html"); //Create or open the file
 
 	std::string name, desc;
 
 
 	std::cout << "Please write your first and last name: ";
-	std::getline(std::cin, name);
+	std::getline(std::cin, name); //Asking for strings so use getline
 	std::cout << "Please describe yourself shortly: ";
 	std::getline(std::cin, desc);
 
